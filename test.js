d379953ae7c44a7b509ce4d0bedc7845a9172fb8
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 18:51:04 -0700

    Going back to how other javascript packages do it, makes it eaiser on everyone :/

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import reduceKeys from "./source"
+import reduceKeys from "./"
 
 test(({equal, end}) => {
   equal(
