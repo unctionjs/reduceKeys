2bd82a8b029466b6ce3aa8f8d8e15ccca5717a8a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 14:56:49 -0700

    The transformer didnt fix this

--- test.ts
+++ test.ts
@@ -1,13 +1,13 @@
 
 import reduceKeys from "./index";
 
-test(() => {
+test("works", () => {
   expect(
     reduceKeys((accumulation) => (current) => `${accumulation}/${current}`)(".")(["a", "b", "c"])
   ).toBe("./0/1/2");
 });
 
-test(() => {
+test("works", () => {
   expect(
     reduceKeys((accumulation) => (current) => `${accumulation}/${current}`)(".")({
       aaa: "a",
